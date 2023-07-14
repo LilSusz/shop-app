ef1da0c7df9ebfe23a625c3dda6abbd53805085b
Liliana Susz <proglilsu@gmail.com>
2023-07-15 00:12:34 +0200

    Add addToCart function

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -19,6 +19,17 @@ const Product = props => {
     return (props.basePrice + foundSize.additionalPrice);
   };
 
+  const addToCart = event => {
+    event.preventDefault();
+
+    console.log('Product summary');
+    console.log('============');
+    console.log('Name:', props.title);
+    console.log('Price:', getPrice());
+    console.log('Size:', currentSize);
+    console.log('Color:', currentColor);
+  };
+
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
@@ -32,7 +43,7 @@ const Product = props => {
           <h2 className={styles.name}>{props.title}</h2>
           <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
+        <form onSubmit={addToCart}>
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
